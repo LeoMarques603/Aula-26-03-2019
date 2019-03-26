88bf879a3b3e7a112061cf1d317a3cfb3769ba74
Leonardo Marques <sandstorm603@gmail.com>
2019-03-26 20:42:54 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/ItemNotaFiscal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ItemNotaFiscal
+    {
+        public string CodigoProduto { get; set; }
+        public int Preco { get; set; }
+        public int Qtd { get; set; }
+        public NotaFiscal NotaFiscal { get; set; }
+        public int TotalItem { get; private set; }
+
+        public ItemNotaFiscal(string codigoProduto, int preco, int qtd)
+        {
+            CodigoProduto = codigoProduto;
+            Preco = preco;
+            Qtd = qtd;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/NotaFiscal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class NotaFiscal
+    {
+        public string CodigoCliente { get; set; }
+        public int ICMS { get; private set; }
+        public int TotalNota { get; private set; }
+        List<ItemNotaFiscal> Items { get; set; }
+
+        public void Inclusao(string codigoProduto, int preco, int qtd)
+        {
+            ItemNotaFiscal inf = new ItemNotaFiscal(codigoProduto, preco, qtd);
+            Items.Add(inf);
+        }
+
+        public decimal CalculoICMS
+        {
+           get
+            {
+                decimal valorICMS = 0;
+
+                foreach (ItemNotaFiscal inf in Items)
+                {
+                    decimal valorICMS = 0;
+                    valorICMS = TotalNota / 4;
+                    
+                }
+                return valorICMS
+            }
+        }
+    }
+}
